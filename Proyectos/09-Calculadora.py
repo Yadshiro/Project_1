069ecd0d153a05e0f66cef5e4dfd3f79104d675b
Yadshiro <yasel.rubioribeaux@web.de>
2024-12-07 09:40:19 +0100

    Added Features

--- Proyectos/09-Calculadora.py
+++ Proyectos/09-Calculadora.py
@@ -5,14 +5,14 @@ print("LAs operaciones son suma, resata, mult, div")
 resultado =""
 while True:
     if not resultado:
-        resultado = input("Ingrese numero: ")
+        resultado = input("Ingrese numero por favor: ")
         if resultado.lower() == "salir":
             break
         resultado = int(resultado)
     op = input("Ingrese operacion: ")
     if op.lower() == "salir":
         break
-    n2 = input("Ingresa siguiente numero: ")
+    n2 = input("Ingresa siguiente numeros: ")
     if n2.lower() =="salir":
         break
     n2 = int(n2)
